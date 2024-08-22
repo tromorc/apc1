b5d145da848e97ca3e6f706a54a07d5434b19c48
tromorc <fausto.jose@iesb.edu.br>
2024-08-22 00:29:04 +0000

    Aula 02

---
+++ praticas/pratica02/imprima_caracteres.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  printf("%c\n", 'A');
+  printf("%i\n", 'A');
+  printf("%s\n", "Eu estou aprendendo a programar em C!");
+  printf("%20s\n", "Café");
+  printf("%-20s\n", "Café");
+  return 0;
+}
