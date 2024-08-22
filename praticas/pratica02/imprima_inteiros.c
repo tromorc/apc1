d68ceeb0f7abae3794a40e810c49e6f3a7df6c47
tromorc <fausto.jose@iesb.edu.br>
2024-08-22 00:03:24 +0000

    Aula 02

---
+++ praticas/pratica02/imprima_inteiros.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  printf("%i\n", 1234);
+  printf("%i\n", 56789);
+  printf("%05i\n", 1);
+  printf("(%i, %i)\n", -1, 1);
+  printf("%02i/%02i/%04i\n", 5, 8, 2024);
+  return 0;
+}
---
+++ praticas/pratica02/imprima_seu_nome.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  printf("Fausto José Alve da Cunha\n");
+  return 0;
+}
