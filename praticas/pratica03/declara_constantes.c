76d349e2e3a8b50cf1d656f4f7e93fc09cef1130
tromorc <fausto.jose@iesb.edu.br>
2024-08-28 13:54:30 +0000

    pratica03

---
+++ praticas/pratica03/declara_constantes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define PI 3.141593
+int main() {
+  // PI = 3.14; Não é permitido //
+  printf("Valor de PI: %f\n", PI);
+  const char SEXO_MASCULINO = 'M';
+  printf("Sexo Masculino: %c\n", SEXO_MASCULINO);
+
+  return 0;
+}
